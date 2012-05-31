99b4fc6bcff305d3a7657cdc8b5b8e3a74ed9b21
Kevin Cantu <me@kevincantu.org>
2012-05-30 23:33:55 -0700

    Simple reader ++

--- knucleotide.rs
+++ knucleotide.rs
@@ -1,13 +1,29 @@
 
+import io::reader_util;
+
 fn main () {
-   import io::reader_util;
 
    io::println("Hello.");
 
-   let rdr: io::reader = io::stdin();
+   let rdr = io::stdin();
+
+   seek_matching_sequence(rdr);
+}
+
+fn seek_matching_sequence(rdr: io::reader) {
    while !rdr.eof() {
       let ln: str = rdr.read_line();
-      io::println("line: " + ln);
+      alt ln[0] {
+         '>' as u8 { alt str::find_str_from(ln, "THREE", 1u) {
+                  option::some(_) { process_sequence(rdr) }
+                  option::none    { }
+             }}
+         _   { }
+      }
    }
+}
+
+fn process_sequence(rdr: io::reader) {
+   io::println("processing...");
 }
 
