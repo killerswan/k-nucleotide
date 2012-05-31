1bb9ae20f8d7beb84f3f3ad3da43a589253612d2
Kevin Cantu <me@kevincantu.org>
2012-05-31 00:27:01 -0700

    make the update function a fn@

--- knucleotide.rs
+++ knucleotide.rs
@@ -2,33 +2,42 @@
 import io::reader_util;
 
 fn main () {
-   let rdr = io::stdin();
-   seek_matching_sequence(rdr);
-}
+   // define leftovers
+   // define maps
 
-fn seek_matching_sequence(rdr: io::reader) {
+   let update_kmer_freqs = fn@(line: str) {
+      // update the frequency maps
+      io::println("GEARS SPINNING...");
+   };
+
    let mut proc_mode = false;
 
-   fn update_kmer_freqs(line: str) {
-      io::println("GEARS SPINNING...");
-   }
+   let rdr = io::stdin();
 
    while !rdr.eof() {
       let line: str = rdr.read_line();
       if str::len(line) == 0u { cont; }
       alt (line[0], proc_mode) {
+
+         // start processing if this is the one
          ('>' as u8, false) {
             alt str::find_str_from(line, "THREE", 1u) {
                option::some(_) { proc_mode = true; }
-               option::none    { }
+               option::none { }
             }
          }
+
+         // break our processing
          ('>' as u8, true) {
             ret;
          }
+
+         // process the sequence for k-mers
          (_, true) {
                update_kmer_freqs(line);
          }
+
+         // whatever
          _ { }
       }
    }
