d067dd4be93ccb51bf59d88f63cbc56a6c6530ae
Kevin Cantu <me@kevincantu.org>
2012-05-31 00:01:14 -0700

    now reading and processing only the sequence of importance...

--- knucleotide.rs
+++ knucleotide.rs
@@ -2,28 +2,36 @@
 import io::reader_util;
 
 fn main () {
-
-   io::println("Hello.");
-
    let rdr = io::stdin();
-
    seek_matching_sequence(rdr);
 }
 
 fn seek_matching_sequence(rdr: io::reader) {
+   let mut proc_mode = false;
+
+   fn update_kmer_freqs(line: str) {
+      io::println("GEARS SPINNING...");
+   }
+
    while !rdr.eof() {
-      let ln: str = rdr.read_line();
-      alt ln[0] {
-         '>' as u8 { alt str::find_str_from(ln, "THREE", 1u) {
-                  option::some(_) { process_sequence(rdr) }
-                  option::none    { }
-             }}
-         _   { }
+      let line: str = rdr.read_line();
+      if str::len(line) == 0u { cont; }
+      alt (line[0], proc_mode) {
+         ('>' as u8, false) {
+            alt str::find_str_from(line, "THREE", 1u) {
+               option::some(_) { proc_mode = true; }
+               option::none    { }
+            }
+         }
+         ('>' as u8, true) {
+            ret;
+         }
+         (_, true) {
+               update_kmer_freqs(line);
+         }
+         _ { }
       }
    }
 }
 
-fn process_sequence(rdr: io::reader) {
-   io::println("processing...");
-}
 
