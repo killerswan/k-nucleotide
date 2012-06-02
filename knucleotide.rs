2535444d4765b8429aca6a9aec1c80c3c365201b
Kevin Cantu <me@kevincantu.org>
2012-06-01 22:10:38 -0700

    hammering away at floats, %f

--- knucleotide.rs
+++ knucleotide.rs
@@ -145,53 +145,118 @@ fn main () {
 
 
 
-// originally from f64.rs
-const epsilon: f64 = 2.2204460492503131e-16_f64;
-
 // originally from float.rs
 // 
-fn my_to_str_common(num: float, digits: uint, exact: bool) -> str {
+fn my_to_str_common(num: float, num_total: uint, num_decimals: uint, exact: bool) -> str {
    import float::*;
 
     if is_NaN(num) { ret "NaN"; }
     if num == infinity { ret "inf"; }
     if num == neg_infinity { ret "-inf"; }
-    let mut (num, accum) = if num < 0.0 { (-num, "-") } else { (num, "") };
+
+    let mut (num, sign) = if num < 0.0 { (-num, "-") } else { (num, "") };
+
+    // truncated integer
     let trunc = num as uint;
+
+    // decimal remainder
     let mut frac = num - (trunc as float);
-    accum += uint::str(trunc);
-    if (frac < epsilon && !exact) || digits == 0u { ret accum; }
-    accum += ".";
-    let mut i = digits;
-    let mut epsilon_prime = 1. / pow_with_uint(10u, i);
-    while i > 0u && (frac >= epsilon_prime || exact) {
+
+    // stack of digits
+    let mut fractionalParts = [];
+
+    // note: this used to return right away without rounding, as "[-]num":
+    //    if (frac < epsilon && !exact) || digits == 0u { ret accum; }
+    // 
+    // but given epsilon like in f64.rs, I don't see how it did much
+    // const epsilon: f64 = 2.2204460492503131e-16_f64;
+
+    let mut ii = num_decimals;
+    let mut epsilon_prime = 1.0 / pow_with_uint(10u, ii);
+
+    // while we still need digits
+    // build stack of digits
+    while ii > 0u && (frac >= epsilon_prime || exact) {
+        // store the next digit
         frac *= 10.0;
-        epsilon_prime *= 10.0;
         let digit = frac as uint;
+        vec::push(fractionalParts, digit);
+
+        // calculate the next frac
         frac -= digit as float;
-
-        // gracefully round when 'exact'
-        if i == 1u && (frac * 10.0) as uint >= 5u {
-            accum += uint::str(digit + 1u);
-        } else {
-            accum += uint::str(digit);
-        }
-
-        i -= 1u;
+        epsilon_prime *= 10.0;
+        ii -= 1u;
     }
 
-    ret accum;
+    let mut acc;
+    let mut racc = "";
+    let mut carry = if frac * 10.0 as uint >= 5u { 1u } else { 0u };
+
+    // turn digits into string
+    // using stack of digits
+    while vec::len(fractionalParts) > 0u {
+         let mut adjusted_digit = carry + vec::pop(fractionalParts);
+
+         if adjusted_digit == 10u { carry = 1u; adjusted_digit %= 10u } else { carry = 0u };
+
+         racc = uint::str(adjusted_digit) + racc;
+    }
+
+    // pad decimals with trailing zeroes
+    // given our precision problems this may be dead code
+    while str::len(racc) < num_decimals && exact {
+        racc += "0"
+    }
+
+    // combine ints and decimals
+    let mut ones = uint::str(trunc + carry);
+    if racc == "" {
+       acc = ones;
+    } else {
+       acc = ones + "." + racc;
+    }
+
+    // pad leading ones
+    while str::len(acc) < num_total {
+        acc = " " + acc
+    }
+
+    acc = sign + acc;
+   
+    ret acc;
 }
 
 // originally from float.rs
 fn my_to_str_exact(num: float, digits: uint) -> str {
-    my_to_str_common(num, digits, true)
+    my_to_str_common(num, 0u, digits, true)
 }
 
 #[test]
 fn rounding() {
-   io::println(float::to_str(3.14159, 4u));
-   io::println(my_to_str_exact(3.14159, 4u));
+   // trailing zeroes should be inserted, or rather
+   // this dodgy floating point stuff should be rounded
+   assert "3.141589999" == #fmt["%9.9f", 3.14159];
+   assert "3.141589999" == float::to_str_common(3.14159, 9u, false);
+   assert "3.141590000" == my_to_str_common(3.14159, 0u, 9u, false);
+
+   assert "3.14158"     == float::to_str_common(3.14159, 5u, false);
+   assert "3.14159"     == my_to_str_common(3.14159, 0u, 5u, false);
+   
+   // this truncation should be rounded
+   assert "3.1415" == float::to_str(3.14159, 4u);
+   assert "3.1416" == my_to_str_exact(3.14159, 4u);
+   assert "3"      == my_to_str_exact(3.14159, 0u);
+   assert "17"     == my_to_str_exact(16.9, 0u);
+   assert "7.0000" == my_to_str_exact(6.99999999, 4u);
+
+   // FIXME: epsilon calc should be adjusted and we should round these accordingly
+   assert "3.14158999999999988261" == float::to_str_common(3.14159, 20u, false);
+   assert "3.14158999999999988262" == my_to_str_exact(3.14159, 20u);
+   assert "3.14158999999999988262" == my_to_str_common(3.14159, 0u, 20u, false);
+
+   // total space padding
+   assert "   3.141590000" == my_to_str_common(3.14159, 14u, 9u, false);
+   assert "   3.141589999" == #fmt["%14.9f", 3.14159];
 }
 
 
